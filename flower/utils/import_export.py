d2f3f7d73b0d1650976d287904f070593d62d475
Gerke van Garderen <info@gerkevangarderen.nl>
2024-11-01 17:50:19 +0100

    Functional import/export for tasks
    Signed-off-by: Gerke van Garderen <info@gerkevangarderen.nl>

--- flower/utils/import_export.py
+++ flower/utils/import_export.py
@@ -1,74 +1,74 @@
 import json
+
 from flower.events import EventsState
 
-def export_tasks_and_workers(events_state):
+def export_tasks_and_workers(events_state: EventsState):
     data = {
-        "tasks": {},
-        "workers": []
+        "tasks": []
     }
 
-    for task_id, task in events_state.tasks.items():
-        data["tasks"][task_id] = task.as_dict()
-        data["tasks"][task_id]['worker'] = task.worker.hostname if task.worker else None
-
-    for worker_name, worker in events_state.workers.items():
-        data["workers"].append({
-            "name": worker_name,
-            "hostname": worker.hostname,
-            "pid": worker.pid,
-            "freq": worker.freq,
-            "heartbeats": worker.heartbeats,
-            "clock": worker.clock,
-            "active": worker.active,
-            "processed": worker.processed,
-            "loadavg": worker.loadavg,
-            "sw_ident": worker.sw_ident,
-            "sw_ver": worker.sw_ver,
-            "sw_sys": worker.sw_sys
-        })
+    for _, task in events_state.tasks.items():
+        task_data = task.as_dict()
+        task_data['worker'] = task.worker.hostname if task.worker else None
+        data["tasks"].append(task_data)
+    # for worker_name, worker in events_state.workers.items():
+    #     data["workers"].append({
+    #         "name": worker_name,
+    #         "hostname": worker.hostname,
+    #         "pid": worker.pid,
+    #         "freq": worker.freq,
+    #         "heartbeats": worker.heartbeats,
+    #         "clock": worker.clock,
+    #         "active": worker.active,
+    #         "processed": worker.processed,
+    #         "loadavg": worker.loadavg,
+    #         "sw_ident": worker.sw_ident,
+    #         "sw_ver": worker.sw_ver,
+    #         "sw_sys": worker.sw_sys
+    #     })
 
     return json.dumps(data)
 
-def import_tasks_and_workers(events_state, data):
-    # data = json.loads(data)
+def import_tasks_and_workers(events_state: EventsState, data):
+
 
     for task_data in data["tasks"]:
-        print(f"Importing task {task_data.get('id')}")
-        task = events_state.tasks.get(task_data["id"])
-        if not task:
-            print("Creating new task")
-            task = events_state.Task()
-            events_state.tasks[task_data["id"]] = task
+        task_id = task_data["uuid"]
 
-        task.name = task_data["name"]
-        task.args = task_data["args"]
-        task.kwargs = task_data["kwargs"]
-        task.result = task_data["result"]
-        task.state = task_data["state"]
-        task.received = task_data["received"]
-        task.started = task_data["started"]
-        task.succeeded = task_data["succeeded"]
-        task.failed = task_data["failed"]
-        task.retried = task_data["retried"]
-        task.retries = task_data["retries"]
-        task.worker = events_state.Worker(task_data["worker"]) if task_data["worker"] else None
-        task.traceback = task_data["traceback"]
+        if task_id:
+            print(f"Importing task {task_id}")
+        else:
+            print("Importing task with no id, skipping")
+            continue
 
-    for worker_data in data["workers"]:
-        worker = events_state.workers.get(worker_data["name"])
-        if not worker:
-            worker = events_state.Worker(worker_data["name"])
-            events_state.workers[worker_data["name"]] = worker
+        (task, is_new) = events_state.get_or_create_task(task_data["uuid"])
+        if is_new:
+            for field in task._fields:
+                print(f"Setting {field} to {task_data.get(field)}")
+                setattr(task, field, task_data.get(field))
+                print(f"Set {field} to {getattr(task, field)}")
+            
 
-        worker.hostname = worker_data["hostname"]
-        worker.pid = worker_data["pid"]
-        worker.freq = worker_data["freq"]
-        worker.heartbeats = worker_data["heartbeats"]
-        worker.clock = worker_data["clock"]
-        worker.active = worker_data["active"]
-        worker.processed = worker_data["processed"]
-        worker.loadavg = worker_data["loadavg"]
-        worker.sw_ident = worker_data["sw_ident"]
-        worker.sw_ver = worker_data["sw_ver"]
-        worker.sw_sys = worker_data["sw_sys"]
+            if task_data["worker"]:
+                (worker, _) = events_state.get_or_create_worker(task_data["worker"])
+                task.worker = worker
+
+
+    # for worker_data in data.get("workers", []):
+    #     worker = events_state.workers.get(worker_data["name"])
+    #     if not worker:
+    #         worker = events_state.Worker(worker_data["name"])
+    #         events_state.workers[worker_data["name"]] = worker
+
+    #     worker.hostname = worker_data["hostname"]
+    #     worker.pid = worker_data["pid"]
+    #     worker.freq = worker_data["freq"]
+    #     worker.heartbeats = worker_data["heartbeats"]
+    #     worker.clock = worker_data["clock"]
+    #     worker.active = worker_data["active"]
+    #     worker.processed = worker_data["processed"]
+    #     worker.loadavg = worker_data["loadavg"]
+    #     worker.sw_ident = worker_data["sw_ident"]
+    #     worker.sw_ver = worker_data["sw_ver"]
+    #     worker.sw_sys = worker_data["sw_sys"]
 
